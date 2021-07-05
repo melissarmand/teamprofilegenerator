ef884ec8cf4fb7a36f88a191d80063634b7e85af
Melissa Armand <melissarmand@gmail.com>
2021-07-04 22:27:38 -0400

    updated file structure, beginning of tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee class", () => {
+    it("Should include an email address", () => {
+
+        
+        expect().toBe();
+    });
+}); //these should go at the very end
