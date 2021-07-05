906b74fa8f925d87be86d6070a1d8d9812afd6a4
Melissa Armand <melissarmand@gmail.com>
2021-07-05 19:03:20 -0400

    changes added to the employee.js file

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ function appMenu() {
 
   function buildTeam() {
     // Create the output directory if the output path doesn't exist
-    if (!fs.existsSync(OUTPUT_DIR)) {
+    if (!fs.existsSync()) {
       fs.mkdirSync(OUTPUT_DIR)
     }
     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
