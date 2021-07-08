bd9318b729d1b5ff49121d8692eaf1c517eaee50
Melissa Armand <melissarmand@gmail.com>
2021-07-08 07:00:42 -0400

    finished up functions for the lib

--- index.js
+++ index.js
@@ -238,9 +238,11 @@ function appMenu() {
 
   function buildTeam() {
     // Create the output directory if the output path doesn't exist
-    
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR)
+    }
     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-  }
+  };
 
   createManager();
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-function createTeam(answers) {
+function render(answers) {
 
     return ''
 }
